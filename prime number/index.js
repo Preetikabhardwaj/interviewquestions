46366c70795525496ba5dacc50157d4a125cef2d
Preetikabhardwaj <preetikabhardwaj2001@gmail.com>
2023-01-25 21:33:55 +0530

    w i p

---
+++ prime number/index.js
@@ -0,0 +1,9 @@
+function primenumber() {
+  let input = parseInt(prompt("please enter an integer value"));
+  for (i = input; i <= input; i++) {
+    if ((i % 1, 0, i == 0)) {
+      console.log(i);
+    } else {
+    }
+  }
+}
