ac8469a68d3ac0e467dfaa19e981bc0a05be7f42
Preetikabhardwaj <preetikabhardwaj2001@gmail.com>
2023-01-27 22:19:54 +0530

    done

--- prime number/index.js
+++ prime number/index.js
@@ -1,9 +1,25 @@
 function primenumber() {
+  let count = 0;
   let input = parseInt(prompt("please enter an integer value"));
-  for (i = input; i <= input; i++) {
-    if ((i % 1, 0, i == 0)) {
-      console.log(i);
-    } else {
+  if (input < 2) {
+    let div = document.createElement("div");
+    div.innerHTML = `  ${input} is not a prime number`;
+    let output = document.getElementById("output").appendChild(div);
+  } else {
+    for (let i = 2; i <= input; i++) {
+      if (input % i == 0) {
+        count++;
+      }
     }
+
+    let div = document.createElement("div");
+
+    if (count == 1) {
+      div.innerHTML = `  ${input} is prime number`;
+    } else {
+      div.innerHTML = `  ${input} is not a prime number`;
+    }
+
+    let output = document.getElementById("output").appendChild(div);
   }
 }
