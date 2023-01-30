a5ba39b17f3a99d0e5dae5b2264fb815bf430ad6
Preetikabhardwaj <preetikabhardwaj2001@gmail.com>
2023-01-30 22:39:07 +0530

    pendind

--- sum with do while loop/index.js
+++ sum with do while loop/index.js
@@ -1,8 +1,14 @@
-function sum(){
-    let sum;
-    let num1= document.getElementById("num1").value
-    let num2 = document.getElementById("num2").value;
-    do {
-        sum= num1+num2;i++;
-    } while()
+function sum() {
+  let sum;
+  let i = 0;
+  let output = document.getElementById("output");
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+  do {
+    sum = num1 + num2;
+    i++;
+    let div = document.createElement("div");
+    div.innerHTML = `${sum}`;
+    output.appendChild(div);
+  } while (i <= num1 && num2);
 }
