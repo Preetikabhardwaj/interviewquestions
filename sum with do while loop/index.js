3e2d476c4d1464627f2282479982991372b8fe80
Preetikabhardwaj <preetikabhardwaj2001@gmail.com>
2023-02-01 12:48:44 +0530

    completed

--- sum with do while loop/index.js
+++ sum with do while loop/index.js
@@ -1,27 +1,25 @@
 function sum() {
   var sum;
   let i = 0;
-
   let num1 = parseInt(document.getElementById("num1").value);
   let num2 = parseInt(document.getElementById("num2").value);
+  let output = document.getElementById("output");
   let newdiv = (document.getElementById("newdiv").style.display = "block");
+
   do {
     sum = num1 + num2;
     i++;
     let div = document.createElement("div");
     div.innerHTML = `${sum}`;
-    let output = document.getElementById("output").appendChild(div);
+    output.appendChild(div);
   } while (i < 1);
 }
 function repeatsum() {
-  let num1 = document.getElementById("num1");
-  let num2 = document.getElementById("num2");
-  num1.value = "";
-  num2.value = "";
+  document.getElementById("num1").value = "";
+  document.getElementById("num2").value = "";
   document.getElementById("newdiv").style.display = "none";
   document.getElementById("output").removeChild(output.firstElementChild);
 }
-function dontrepeat(){
-   document.getElementById("newdiv").style.display = "none";
-
+function dontrepeat() {
+  document.getElementById("newdiv").style.display = "none";
 }
