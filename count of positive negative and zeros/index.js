62caca3f99e32e22e648f1edae1e76dac344f479
Preetikabhardwaj <preetikabhardwaj2001@gmail.com>
2023-02-02 15:18:49 +0530

    almost done

--- count of positive negative and zeros/index.js
+++ count of positive negative and zeros/index.js
@@ -2,20 +2,29 @@ function countofnpz() {
   let input = parseInt(document.getElementById("input").value);
   let output = document.getElementById("output");
   let i;
-  let positive;
-  let negative;
-  let zeros;
-  for (i = 0; i <= input; i++) {
+  let positive = 0;
+  let negative = 0;
+  let zeros = 0;
+  for (i = 1; i <= input; i++) {
     let div = document.createElement("div");
     div.innerHTML = `${i}`;
     output.appendChild(div);
     console.log(i);
   }
   if (input < 0) {
-    console.log("negative number");
-  } else if ((input = i)) {
-    console.log("zeros number");
+    negative = negative + input;
+    let div = document.createElement("div");
+    div.innerHTML = `${negative} is negative number`;
+    output.appendChild(div);
+  } else if (input <= 0) {
+    zeros = zeros + input;
+    let div = document.createElement("div");
+    div.innerHTML = `${zeros} is  zero`;
+    output.appendChild(div);
   } else {
-    console.log("positive number");
+    positive = positive + input;
+    let div = document.createElement("div");
+    div.innerHTML = ` ${input} is positive ${positive} is  positive`;
+    output.appendChild(div);
   }
 }
