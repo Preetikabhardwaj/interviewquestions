ecf62bfb58c1f7c5b5ba6cf6727fde7fda14c94d
Preetikabhardwaj <preetikabhardwaj2001@gmail.com>
2023-02-03 12:44:05 +0530

    done

---
+++ largest and smallest number/index.js
@@ -0,0 +1,13 @@
+function getnumbers() {
+  let input = parseInt(document.getElementById("input").value);
+  let input2 = parseInt(document.getElementById("input2").value);
+  let output = document.getElementById("output");
+  for (let i = 0; i <= input2; i++) {
+    let div = document.createElement("div");
+    div.innerHTML = `${i} `;
+    output.appendChild(div);
+  }
+  let div = document.createElement("div");
+  div.innerHTML = `${input} is smallest number ${input2} is largest number`;
+  output.appendChild(div);
+}
