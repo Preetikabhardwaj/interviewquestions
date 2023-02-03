bdc0d60d5b313683dc2d916db9bcddabf120b635
Preetikabhardwaj <preetikabhardwaj2001@gmail.com>
2023-02-03 13:06:42 +0530

    done

---
+++ armstrong number/index.js
@@ -0,0 +1,15 @@
+function getarmstrongnumber() {
+  let input = parseInt(document.getElementById("input").value);
+  let output = document.getElementById("output");
+  for (let i = input; i <= 500; i++) {
+    let div = document.createElement("div");
+    if (i * i * i == i) {
+      div.innerHTML = `${i} is an armstrong number`;
+      output.appendChild(div);
+    } else {
+      let div = document.createElement("div");
+      div.innerHTML = `${i}`;
+      output.appendChild(div);
+    }
+  }
+}
